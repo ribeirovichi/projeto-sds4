674a6c8dbca31fbf99a18f44ff9a2730b4baf2f9
Raphael Ribeirao <ribeirovichi@gmail.com>
2021-09-09 02:19:30 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.List;
 @Entity
 @Table(name = "tb_sellers")
 public class Seller {
