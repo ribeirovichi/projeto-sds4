1c0f7391cd6309504e400b5f68b5bc331fed5475
Raphael Ribeirao <ribeirovichi@gmail.com>
2021-09-09 00:59:52 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
+import java.util.List;
 @Entity
 @Table(name = "tb_sellers")
 public class Seller {
